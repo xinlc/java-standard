446f2319e71646e0dc0d3d1748a32bd2ec804fce
Leo <xinlichao2016@gmail.com>
2019-12-10 09:49:15 +0800

    init

---
+++ src/main/java/com/leo/javastandard/JavaStandardApplication.java
@@ -0,0 +1,13 @@
+package com.leo.javastandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStandardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/javastandard/JavaStandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.javastandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
